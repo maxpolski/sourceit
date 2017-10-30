22a7ad2f0fe053fe56cfe9eb16f8a2c0edbfdb1c
lol <misha@lol.lol>
2017-10-30 12:40:52 +0200

    added corrected hw6

--- lectures/ES6Classes/HW/Polina.Neier/ES6Classes.checked.js
+++ lectures/ES6Classes/HW/Polina.Neier/ES6Classes.checked.js
@@ -1,61 +1,61 @@
 // зачем столько отступов в начале? Много ошибок eslint
-        "use strict";
+'use strict';
 
-        class Machine {
-            constructor(name, type) {
-                this.name = name;
-                this.type = type;
-            }
+class Machine {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
 
-            turnOn() {
-                console.log(this.name + ' is on');
-            }
+    turnOn() {
+        console.log(this.name + ' is on');
+    }
 
-            turnOff() {
-                console.log(this.name + ' is off');
-            }
-        }
+    turnOff() {
+        console.log(this.name + ' is off');
+    }
+}
 
-        class CoffeMachine extends Machine {
-            constructor(name, type, numOfPrograms, manufacturer) {
-                super(name, type);
-                this.numOfPrograms = numOfPrograms;
-                this.manufacturer = manufacturer;
-            }
+class CoffeMachine extends Machine {
+    constructor(name, type, numOfPrograms, manufacturer) {
+        super(name, type);
+        this.numOfPrograms = numOfPrograms;
+        this.manufacturer = manufacturer;
+    }
 
-            makeCoffe() {
-                console.log('Coffe please');
-            }
+    makeCoffe() {
+        console.log('Coffe please');
+    }
 
-            makeLatte() {
-                console.log('Latte please');
-            }
-        }
+    makeLatte() {
+        console.log('Latte please');
+    }
+}
 
-        class Microwave extends Machine {
-            constructor(name, type, dishes) {
-                super(name, type);
-                this.dishes = dishes;
-            }
+class Microwave extends Machine {
+    constructor(name, type, dishes) {
+        super(name, type);
+        this.dishes = dishes;
+    }
 
-            cookDish(nameOfDish, time) {
-                let currentDish = this.dishes.find((elem, ind, arr) => { return (nameOfDish === elem.nameOfDish) });
-                setTimeout(function () { alert(currentDish.nameOfDish + ' is ready!') }, currentDish.time * 1000);
-            }
-        }
-
-
+    cookDish(nameOfDish) {
+        let currentDish = this.dishes.find((elem) => { return (nameOfDish === elem.nameOfDish); });
+        setTimeout(function () { alert(currentDish.nameOfDish + ' is ready!'); }, currentDish.time * 1000);
+    }
+}
 
 
-        const machine = new Machine('super', 'puper');
-        const coffemachine = new CoffeMachine('super1', 'puper1', 3, 'dell');
-        const microwave = new Microwave('s2', 'p2', [{ nameOfDish: 'Borshch', time: 30 }, { nameOfDish: 'Ukha', time: 40 }]);
 
-        microwave.cookDish('Ukha');
 
-        coffemachine.turnOn();
-        coffemachine.makeLatte();
-        machine.turnOn();
-        machine.turnOff();
-        microwave.turnOn();
+const machine = new Machine('super', 'puper');
+const coffemachine = new CoffeMachine('super1', 'puper1', 3, 'dell');
+const microwave = new Microwave('s2', 'p2', [{ nameOfDish: 'Borshch', time: 30 }, { nameOfDish: 'Ukha', time: 40 }]);
+
+microwave.cookDish('Ukha');
+
+coffemachine.turnOn();
+coffemachine.makeLatte();
+machine.turnOn();
+machine.turnOff();
+microwave.turnOn();
 
