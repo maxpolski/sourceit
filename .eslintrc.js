f764c07ad9aa48b9ef8b6fd6abf95658b2490240
Max Korabekskyi <max_polski@icloud.com>
2017-10-30 13:06:33 +0200

    rolled back eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,6 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
+    "extends": "airbnb-base",
     "rules": {
-        "no-console":0,
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
+        "linebreak-style": 0
     }
 };
