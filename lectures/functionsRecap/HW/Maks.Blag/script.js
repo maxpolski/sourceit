04dee95bc6b1a893d4d7ba266528b827b40133a5
blaghour <hourblag@gmail.com>
2017-11-18 03:10:11 +0200

    rewrite script

--- lectures/functionsRecap/HW/Maks.Blag/script.js
+++ lectures/functionsRecap/HW/Maks.Blag/script.js
@@ -1,28 +1,23 @@
-function createCounter (startCount, incrementor){
-    //var counter = startCount || 0;  
-    //var increment = incrementor || 1;
-    this.counter = startCount || 0;  
-    this.increment = incrementor || 1;
-        return () => {
-            console.log(this.counter);
-            this.counter += this.increment;
-            //console.log(counter);
-            //counter += increment;
-    }
-};
+function createCounter(startCount, incrementor) {
+  this.counter = startCount || 0;
+  this.increment = incrementor || 1;
+  return () => {
+    console.log(this.counter);
+    this.counter += this.increment;
+  };
+}
 
 const counter = createCounter();
-counter() 
-counter() 
-counter() 
+counter();
+counter();
+counter();
 
-const counterFoo = createCounter(1);
-counter()
-counter()
-counter() 
-//console.log(counterFoo)
+createCounter(1);
+counter();
+counter();
+counter();
 
-const counterBoo = createCounter(50, 25);
-counter()
-counter()
-counter()
+createCounter(50, 25);
+counter();
+counter();
+counter();
