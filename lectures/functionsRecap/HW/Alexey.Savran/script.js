9282253c84bdca5297e748de00bb9d038392c481
SavranAlexey <savran333@gmail.com>
2017-11-19 19:32:37 +0200

    homework function recap

---
+++ lectures/functionsRecap/HW/Alexey.Savran/script.js
@@ -0,0 +1,23 @@
+function createCounter(startCount, incrementor) {
+  this.count = startCount;
+  this.incrementor = incrementor;
+
+  if (startCount === undefined) {
+    this.count = 0;
+  }
+
+  if (incrementor === undefined) {
+    this.incrementor = 1;
+  }
+
+  return () => {
+    console.log(this.count);
+    this.count += this.incrementor;
+  };
+}
+
+const counter = createCounter(50, 25);
+counter();
+counter();
+counter();
+
