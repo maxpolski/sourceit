8fcb200081e111e5cbaf75944673830b3579b7a0
Maksym Korabelskyi <maksymkorabelskyi@Maksyms-MacBook-Air.local>
2017-11-24 18:57:27 +0200

    add react example

---
+++ lectures/React/example/src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const node = document.getElementById('app');
+
+render(
+  <div>Hello world</div>,
+  node
+);
---
+++ lectures/React/example/webpack.config.babel.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+export default {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules)/,
+    }],
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: './index.html' }]),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
+};
