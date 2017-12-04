c68df8b1b7d23237cff1c363c4ddbbbfcf1547a2
Maksym Korabelskyi <maksymkorabelskyi@Maksyms-MacBook-Air.local>
2017-12-04 19:33:12 +0200

    add server to redux

---
+++ lectures/Redux/example/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(express.static(`${path.resolve(__dirname)}/dist`));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.use(bodyParser.json());
+
+const todos = [{
+  id: 1,
+  title: 'Buy milk',
+  isCompleted: true,
+}, {
+  id: 2,
+  title: 'Repair computer',
+  isCompleted: false,
+}];
+
+let lastTodoIndex = todos[todos.length - 1].id;
+
+app.get('/', (req, res) => {
+  const indexFile = fs.readFileSync(`${path.resolve(__dirname)}/index.html`);
+  res.set('Content-Type', 'text/html');
+  res.send(indexFile);
+});
+
+app.get('/todos', (req, res) =>
+  res
+    .set('Content-Type', 'application/json')
+    .json(todos));
+
+app.post('/todo', (req, res) => {
+  const newTodo = req.body.todo;
+  lastTodoIndex += 1;
+  todos.push(Object.assign({}, newTodo, { id: lastTodoIndex, isCompleted: false }));
+  res.end();
+});
+
+app.put('/todo', (req, res) => {
+  let {
+    id: updatedTodoId,
+    isCompleted,
+    title,
+  } = req.body.todo;
+
+  const todoToUpdateIndex = todos.findIndex(({ id }) => id === updatedTodoId);
+
+  if (typeof isCompleted === 'undefined') {
+    isCompleted = todos[todoToUpdateIndex].isCompleted;
+  }
+  title = title || todos[todoToUpdateIndex].title;
+
+  todos[todoToUpdateIndex] = Object.assign({}, todos[todoToUpdateIndex], { isCompleted, title });
+  res.end();
+});
+
+app.listen('3000', () => console.log('app listening on port 3000'));
